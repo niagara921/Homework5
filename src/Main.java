2df25401b9c06e249f657a36af2360049b7cc833
Татьяна <niagara921@mail.ru>
2022-08-29 23:30:18 +0500

    Домашнее задание 5.1

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,25 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-         int[] keys = new int [3];
-         double[] jams = {1.57, 7.654, 9.936};
-         int[] apartments = new int[4];
-         // Задача 2
+        int[] keys = new int[3];
+        double[] jams = {1.57, 7.654, 9.936};
+        int[] apartments = new int[4];
+        // Задача 2
+        int keysOne = 1;
+        int keysTwo = 2;
+        int keysThree = 3;
 
-        int key = 1;
-        int twoKey = 2;
-        int threeKey = 3;
-        keys[0]= key;
-        keys[1] = twoKey;
-        keys[2] = threeKey;
-        System.out.println(" ");
-        double jamOne = 1.57;
-        double jamTwo = 7.654;
-        double jamThree =9.936;
-        jams[0] = jamOne;
-        jams[1] = jamTwo;
-        jams[2] = jamThree;
+        keys[0] = keysOne;
+        keys[1] = keysTwo;
+        keys[2] = keysThree;
+
+        double jamsOne = 1.57;
+        double jamsTwo = 7.654;
+        double jamsThree = 9.936;
+        jams[0] = jamsOne;
+        jams[1] = jamsTwo;
+        jams[2] = jamsThree;
+
         int apartmentOne = 1;
         int apartmentTwo = 2;
         int apartmentThree = 3;
@@ -29,79 +30,78 @@ public class Main {
         apartments[1] = apartmentTwo;
         apartments[2] = apartmentThree;
         apartments[3] = apartmentFour;
-        for (int i = 0; i < keys.length; i++ ) {
+
+        for (int i = 0; i < keys.length; i++) {
             System.out.print(keys[i]);
-            if (keys[i] < keys.length) {
+            if (i < keys.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println(" ");
 
-        for(int b = 0; b< jams.length; b++){
+        for (int b = 0; b < jams.length; b++) {
             System.out.print(jams[b]);
-                if(jams[b]< jams.length){
-                    System.out.print( ", ");
-                }
+            if (b < jams.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println(" ");
+
         int f = 0;
-        for(; f < apartments.length; f++) {
+        for (; f < apartments.length; f++) {
             System.out.print(apartments[f]);
-                if(apartments[f] < apartments.length){
-                    System.out.print( ", ");
-               }
+            if (f < apartments.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println(" ");
 
         //Задача 3
 
-        for( int s = keys.length-1; s>=0; s--) {
+        for (int s = keys.length - 1; s >= 0; s--) {
             System.out.print(keys[s]);
-            if (keys[s] > keys[0]) {
-                System.out.print( ", ");
-            }
-        }
-        System.out.println(" ");
-        for (int z = jams.length-1;z >= 0; z--) {
-            System.out.print(jams[z]);
-                if (jams[z] > jams[0]) {
-                    System.out.print(", ");
-            }
-        }
-        System.out.println(" ");
-
-        for(int d = apartments.length-1; d >= 0; d--){
-            System.out.print(apartments[d]);
-            if (apartments[d] > apartments[0]){
+            if (s != 0) {
                 System.out.print(", ");
             }
         }
         System.out.println(" ");
-           //задача 4
-        for (int v = 1; v < keys.length; v++){
-            if (keys[v] % 2 != 0){
+        {
+
+        }
+
+        for (int z = jams.length - 1; z >= 0; z--) {
+            System.out.print(jams[z]);
+            if (z != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(" ");
+        {
+
+        }
+
+        for (int d = apartments.length - 1; d >= 0; d--) {
+            System.out.print(apartments[d]);
+            if (d != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(" ");
+
+
+        //задача 4
+
+
+        for (int v = 0; v < keys.length; v++) {
+            if (keys[v] % 2 != 0) {
                 keys[v]++;
             }
-            System.out.print(keys[v] + ",");
-        }
-         // Задача 5
-          int[] weights = {90, 93, 51};
-          int[] weightsCopy = {90, 93, 51};{
-              if(weights == weightsCopy){
-                  System.out.println("Массивы одинаковые");
-              }else{
-                  System.out.println("Массивы разные");
-              }
-            for (int y = 0; y < weightsCopy.length; y++) {
-                System.out.println(weightsCopy[y]);
-
+            System.out.print(keys[v]);
+            if (v < keys.length - 1) {
+                System.out.print(", ");
             }
         }
-
-
-
-
-
-
     }
 }
+
+
